483e08905fae6eb465307034a95501286c4e5538
suyog <ersuyog16@gmail.com>
2024-05-05 20:37:29 +0530

    changed url for backend request

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import React from "react";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import "bootstrap/dist/css/bootstrap.css";
+
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import CreateUser from "./components/createUser";
+
+function App() {
+  return (
+    <Router>
+      <div className='App'>
+        <header className='App-header'>
+          {/* Fixed Navbar */}
+          <div className='fixed-top-navbar'>
+            {" "}
+            <Navbar
+              bg='dark'
+              variant='dark'
+              className='fixed-top-navbar'
+              style={{
+                position: "fixed",
+                top: "0",
+                width: "100%",
+                zIndex: "1000",
+              }}>
+              <Container className='d-flex justify-content-center'>
+                <Navbar.Brand style={{ fontWeight: "bold", fontSize: "30px" }}>
+                  News Admin Portal
+                </Navbar.Brand>
+              </Container>
+            </Navbar>
+          </div>
+        </header>
+        <br />
+        <div className='content-wrapper' style={{ paddingTop: "80px" }}>
+          {" "}
+          <div style={{ marginLeft: "30px", marginRight: "30px" }}>
+            <CreateUser />
+          </div>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
---
+++ src/components/createUser.js
@@ -0,0 +1,146 @@
+import React, { useState } from "react";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import axios from "axios";
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import { BsCalendar } from "react-icons/bs"; // Import BsCalendar icon from react-icons/bs
+
+function CreateUser() {
+  const [Title, setTitle] = useState("");
+  const [ImageUrl, setImageUrl] = useState("");
+  const [Description, setDescription] = useState("");
+  const [ReadMoreUrl, setReadMoreUrl] = useState("");
+  const [ReadMoreText, setReadMoreText] = useState("");
+  const [formDate, setFormDate] = useState(null);
+  const [formTime, setFormTime] = useState(null);
+
+  const onSubmit = (e) => {
+    console.log("submited");
+    e.preventDefault();
+
+    const userObject = {
+      Title,
+      ImageUrl,
+      Description,
+      ReadMoreUrl,
+      ReadMoreText,
+      formDate,
+      formTime,
+    };
+
+    axios
+      .post(
+        "https://express-hello-world-pq27.onrender.com/users/create",
+        userObject
+      )
+      .then((res) => {
+        console.log(res.data);
+        alert("User Information Registered");
+        // Clear form fields after successful submission
+        setTitle("");
+        setImageUrl("");
+        setDescription("");
+        setReadMoreUrl("");
+        setReadMoreText("");
+        setFormDate(null);
+        setFormTime(null);
+      })
+      .catch((error) => {
+        console.error("Error submitting form:", error);
+        // Handle error state or display error message
+      });
+  };
+
+  return (
+    <div className='form-wrapper'>
+      <Form onSubmit={onSubmit}>
+        <Form.Group controlId='Title'>
+          <Form.Label>Title</Form.Label>
+          <Form.Control
+            type='text'
+            value={Title}
+            onChange={(e) => setTitle(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId='ImageUrl'>
+          <Form.Label>ImageUrl</Form.Label>
+          <Form.Control
+            type='text'
+            value={ImageUrl}
+            onChange={(e) => setImageUrl(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId='Description'>
+          <Form.Label>Description</Form.Label>
+          <Form.Control
+            type='text'
+            value={Description}
+            onChange={(e) => setDescription(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId='formDate'>
+          <Form.Label style={{ marginRight: "10px" }}>Select Date :</Form.Label>
+          <DatePicker
+            selected={formDate}
+            onChange={(date) => setFormDate(date)}
+            dateFormat='MMMM d, yyyy'
+            className='form-control'
+            placeholderText='Select a date'
+            style={{ marginBottom: "10px" }}
+          />
+        </Form.Group>
+
+        <Form.Group controlId='formTime'>
+          <Form.Label style={{ marginRight: "10px" }}>Time :</Form.Label>
+          <DatePicker
+            selected={formTime}
+            onChange={(time) => setFormTime(time)}
+            showTimeSelect
+            showTimeSelectOnly
+            timeIntervals={15}
+            dateFormat='h:mm aa'
+            placeholderText='Select a time'
+            className='form-control'
+          />
+        </Form.Group>
+
+        <Form.Group controlId='ReadMoreUrl'>
+          <Form.Label>Read More Url</Form.Label>
+          <Form.Control
+            type='text'
+            value={ReadMoreUrl}
+            onChange={(e) => setReadMoreUrl(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <Form.Group controlId='ReadMoreText'>
+          <Form.Label>Read More Text</Form.Label>
+          <Form.Control
+            type='text'
+            value={ReadMoreText}
+            onChange={(e) => setReadMoreText(e.target.value)}
+            required
+          />
+        </Form.Group>
+
+        <br />
+        <br />
+        <br />
+
+        <Button variant='danger' size='lg' block type='submit'>
+          Submit News
+        </Button>
+      </Form>
+    </div>
+  );
+}
+
+export default CreateUser;
