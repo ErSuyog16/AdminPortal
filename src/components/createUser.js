6586725a5454fd797749b6b009c393112f5a1190
Suyog <89405908+ErSuyog16@users.noreply.github.com>
2024-05-20 14:22:08 +0530

    Update createUser.js

--- src/components/createUser.js
+++ src/components/createUser.js
@@ -31,7 +31,7 @@ function CreateUser() {
 
     axios
       .post(
-        "https://express-hello-world-pq27.onrender.com/users/create",
+        "https://newsserver-1.onrender.com/users/create",
         userObject
       )
       .then((res) => {
